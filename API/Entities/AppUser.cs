255b8c7e38653a6879642366e0546dcf2c153c09
Sivakumar Ganesan <sivag.in@gmail.com>
2023-12-30 16:02:34 -0600

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,4 +5,8 @@ public class AppUser
     public string UserName { get; set; }
 
     public int Id { get; set; }
+
+    public byte[] PasswordHash { get; set; }
+
+    public byte[] PasswordSalt { get; set; }
 }
---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+                    IConfiguration config)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+
+            options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+        return services;
+    }
+}
