e242b2f444f9d127b0d57b51b6832f53e31729e6
Sivakumar Ganesan <sivag.in@gmail.com>
2024-01-03 19:32:54 -0600

    end of section 6

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountservice=inject(AccountService);
+  const toastr=inject(ToastrService);
+
+
+  return accountservice.currentUser$.pipe(
+    map(user=>{
+      if(user)
+        return true;
+      else
+      {
+        toastr.error("Acess Denied");
+        return false;
+      }
+       
+    })
+  );
+};
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  @Output() cancelRegister = new EventEmitter();
+  model:any = {}
+
+  constructor(private accountService: AccountService,private toastr:ToastrService){
+
+  }
+
+  register(){
+    this.accountService.register(this.model).subscribe({
+      next:()=>{
+        this.cancel();
+      },
+      error:error=>this.toastr.error(error.error)
+    })
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+}
